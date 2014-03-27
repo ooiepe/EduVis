0e866cedb756b965fb8da17eaff74205eb75d63b
mmills <m@mpm.me>
2014-03-27 11:03:47 -0400

    cleaned up output for console usage

--- compile.php
+++ compile.php
@@ -16,8 +16,9 @@
       "modules/module_formatting.js",
       
       "modules/module_tool.js",
-      "modules/module_utility.js",
-      "resources/js/jquery-1.10.1.min.js"
+      "modules/module_utility.js"
+      // ,
+      // "resources/js/jquery-1.10.1.min.js"
     );
 
     if (is_writable("EduVis.js")) {
@@ -27,32 +28,32 @@
       
       fwrite($outfile,"//Date Compiled: " . date ("F d Y H:i:s") . "\n");
       
-      $components = "<div><h3>Components Included in Build</h3><ul>";
+      $components = "\nComponents Included in Build\n";
 
       $count = count($parts);
       for ($i = 0; $i < $count; $i++) {
         
         fwrite($outfile, file_get_contents($parts[$i]));
 
-        $components .= "<li>".$parts[$i]."</li>";
+        $components .= "\n-".$parts[$i];
 
       }
 
-      $components .= "</ul></div>";
+      $components .= "\n";
 
       fclose($outfile);
      
       //,$f2,$f3,$f4,$f5,$f6,$f7,$f8,$f9,$f10);
 
-      echo "<h1>---EduVis.js COMPILED--- ". $result ."</h1>";
+      //echo "<h1>---EduVis.js COMPILED--- ". $result ."</h1>";
       echo $components;
 
     }
     else{
-      echo "<h1>---NO COMPILE.. file not writable---</h1>". date ("F d Y H:i:s");
+      echo "NO COMPILE.. file not writable". date ("F d Y H:i:s") ." \n";
     }
 
   }
   else{
-    echo "<h1>---NO COMPILE---</h1>". date ("F d Y H:i:s");
+    echo "NO COMPILE". date ("F d Y H:i:s") . "\n";
   }
